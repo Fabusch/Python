c75530fd522405fd465fe011dc97a8de459bcd21
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-02 09:27:35 +0100

    Python

---
+++ Operationens.py
@@ -0,0 +1,9 @@
+print(2 + 2) #4
+print(5 + 4 - 3) #6
+
+
+print(2 + 2) #4
+print('2' + '2') # Zeichenketten werden immer aneinander gehangen (Ergebnis 22 )
+
+print ( 10 / 2) #5.0            / erzeugt eine Dezimalzahl 
+print (2 * (3 + 4) )
