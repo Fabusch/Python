fba6e36878c235280adf7498f6a72b6b5315975e
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-02 09:33:50 +0100

    ch

--- Datatypes.py
+++ Datatypes.py
@@ -16,3 +16,6 @@ print (2 ** 5)  # The 2 ist the Base Number and the 5 is the index(power)
                 #its like 2^5 in Math 
                 
 print ( 2**3**2) # Sind drei Zahlen vorhanden muss man von RECHTS nach LINKS erstmal 3**2 = 9 dann 2^9 rechnen
+
+
+# The Power = HOCH bsp print ( 5 ** 3)
