b2d66357e30b1c803b72e6c82db2b67e17ad34d8
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-02 09:27:41 +0100

    Python

---
+++ Datatypes.py
@@ -0,0 +1,18 @@
+#Each Value in Python has a type 
+
+"Hello World" #String
+3 # integer
+3.0 # floats 
+
+print ( 8 / 2 ) # erzeugt einen Float Wert
+print ( 6 * 7.0 )
+print ( 4 * 1.65 )
+
+
+
+# Exponention
+
+print (2 ** 5)  # The 2 ist the Base Number and the 5 is the index(power)
+                #its like 2^5 in Math 
+                
+print ( 2**3**2) # Sind drei Zahlen vorhanden muss man von RECHTS nach LINKS erstmal 3**2 = 9 dann 2^9 rechnen
---
+++ Main.py
@@ -0,0 +1,3 @@
+print('Hello World') #print Methode
+print('Mehrere Zeilen sind möglich')
+print('Hello \n ich bins eine neue Linie')
