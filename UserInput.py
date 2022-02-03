78e980ee89f234c19772b628ed4a2dcaeba1b939
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-03 10:41:00 +0100

    pyrth

--- UserInput.py
+++ UserInput.py
@@ -1,15 +1,16 @@
 # input() Takes in a String by default from the User
 #int(input())   takes an Integer
+age = int(input())
+print(age)
 #float(input()) takes a floating point
-
 def f1(e): print(e)
-
 x = input("enter data")
 print(x)
 
-x = input()
-print(x)
-
-
+# Ein Beispiel wie man den Namen abfragen kann.
 name = input ("Enter your name: ")
 print ("Hello, " + name)
+
+#Converting is also Possible 
+age = 42 #Integer 42
+print("His Age is "+ str(age)) #String Age
