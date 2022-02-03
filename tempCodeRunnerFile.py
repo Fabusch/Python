c0b9db30dc00bc938d1f5073d147c576bf069eb8
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-03 09:29:55 +0100

    input

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,4 @@
+
 name = input("Enter your name:")
-print ("Hello, " + name)
+print ("Hello, " + name)    # Not work so good in VSCODE so open the Terminal C:\Workspace\Python python UserInput.py
+                            # and The User Input Works. 
