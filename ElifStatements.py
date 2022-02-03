5c4bd5499df009ad9c343c10d64445ea6cdee9c3
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-03 14:21:50 +0100

    booleanlogik

---
+++ ElifStatements.py
@@ -0,0 +1,12 @@
+#if else if else else else makes a Code to long
+# The Conclusion to this is ELIF
+
+num = 3 
+if num == 1:   #falls
+    print("One")
+elif num == 2: #elif like andersfalls wenn... 
+    print("Two")
+elif num == 3: # andersfalls wenn...
+    print("Three") 
+else:
+    print("Something else")   #Else  falls nicht....
