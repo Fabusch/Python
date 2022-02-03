80d29d99f2b2ee75d31a4e67eed7ae245ad6ea7d
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-03 13:43:27 +0100

    else statements

---
+++ elsestatement.py
@@ -0,0 +1,16 @@
+# The Else Statement can use then some Condition of the IF Statements is false
+
+x = 4 
+if x == 5:
+    print("Yes")
+else:
+    print("no")
+    
+    
+    
+if 1 + 1 == 2:   #true
+    if 2 * 2 == 8: # false
+        print("if") # so this is not true so the compiler goes to next Line
+    else: # next line 
+        print("else") # conclusion 
+        
