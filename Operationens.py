eb78571496598ca8bd9e5436adf41bb92c6a5a8e
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-03 09:05:17 +0100

    python

--- Operationens.py
+++ Operationens.py
@@ -7,3 +7,6 @@ print('2' + '2') # Zeichenketten werden immer aneinander gehangen (Ergebnis 22 )
 
 print ( 10 / 2) #5.0            / erzeugt eine Dezimalzahl 
 print (2 * (3 + 4) )
+
+
+
--- StringOperations.py
+++ StringOperations.py
@@ -2,3 +2,12 @@
 
 print("Bacon" + 'eggs')
 print("2"+"2")  #Warning 22 is the Solution not 4  
+
+
+
+# Its not ablte to add Strings to Integer but u can multiple them like
+print("spam" * 2)
+print(4 * '2')
+
+
+
