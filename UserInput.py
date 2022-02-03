0f206488a2787b5c7856e41cee4321e9b684a81d
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-03 09:29:16 +0100

    input

--- UserInput.py
+++ UserInput.py
@@ -2,7 +2,14 @@
 #int(input())   takes an Integer
 #float(input()) takes a floating point
 
+def f1(e): print(e)
 
+x = input("enter data")
+print(x)
 
 x = input()
 print(x)
+
+
+name = input ("Enter your name: ")
+print ("Hello, " + name)
