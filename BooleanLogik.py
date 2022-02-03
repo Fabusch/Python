dd1cd2778cc41bf75f3978447ec89f79736851c9
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-03 14:21:56 +0100

    booleanlogik

---
+++ BooleanLogik.py
@@ -0,0 +1,27 @@
+#Boolean Operators can check with alot of  Operators
+# mit der and and / und Verbindung
+print (1 == 1 and 2 == 2 ) #true
+
+print (1 == 1 and 2 == 3 ) #False
+
+# Der OR Operator ist True wenn eine(oder beide) True sind    und False wenn beide Bedingungen false sind
+
+
+print ( 1 == 1 or 2 == 3) #True
+
+print (2 < 1 or 3 > 6 ) #false 
+
+
+#The NOT Operator say its false if the normaly conclusion is true und umgekehrt
+
+print(not 1 == 1 ) # False
+print(not 1 > 7 ) #True 
+
+
+if not True:
+    print("1")
+elif not (1+1 ==3):
+    print("2")
+else:
+    print("3")
+    #Conclcusion is 2  because 1+1(2) is not equal to 3.
--- elsestatement.py
+++ elsestatement.py
@@ -1,5 +1,8 @@
 # The Else Statement can use then some Condition of the IF Statements is false
 
+from re import X
+
+
 x = 4 
 if x == 5:
     print("Yes")
@@ -13,4 +16,29 @@ if 1 + 1 == 2:   #true
         print("if") # so this is not true so the compiler goes to next Line
     else: # next line 
         print("else") # conclusion 
-        
+
+
+#Aber Achtung! Es kann immer nur ein Else Statement mit einem IF Statement verknüpft werden 
+#Möchte man mehrer Dinge vergleichen geht dies so
+
+num = 3
+if num ==1:
+    print("One")
+else:
+    if num == 2:
+        print("Two")
+    else:
+        if num == 3:
+            print("Three")
+        else:
+            print("Something else")
+            
+            
+#Another Example
+
+x = 10
+y = 20
+if x > y:
+    print("if statement")
+else:
+    print("else Statement")
