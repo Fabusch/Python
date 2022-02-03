f75aca04efd97ba438b20531c57e7cf9de3f2b73
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-03 13:43:03 +0100

    if statements

---
+++ ifstatements.py
@@ -0,0 +1,27 @@
+# if condition:
+        #statements
+        
+        
+        
+x = 42
+if x > 5:
+    print("x is greather than 5")
+    
+    
+    num = 12
+    if num > 5:
+        print("Bigger than 5")
+        if num <= 47:
+            print("Between 5 and 47")
+            
+            
+            
+num = 7
+if num > 3:
+    print("3")
+    if num < 5:
+        print("5")
+        if num ==7:
+            print("7")       
+            # Maybe u think the Conlusion must be 37 BUT if a Statement FALSE here if num < 5 the Condition stops
+            # so the statement if num == 7: will never executed so is the Conclusion 3.
