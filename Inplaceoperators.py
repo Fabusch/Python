20767603bbe01fae0d9418001e5dce2599009cff
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-03 10:41:06 +0100

    pyth

---
+++ Inplaceoperators.py
@@ -0,0 +1,10 @@
+#Never Write =+   ,   += is the Way to not Produce Bugs
+x = 2
+x += 3
+print (x)
+
+
+
+y = 4
+y *= 3          # its like y(4) * 3          // So spart man sich das inijizieren einer neuen Variable !  ! ! 
+print(y)
---
+++ WorkingwithInput.py
@@ -0,0 +1,13 @@
+x = "2"
+y = "4"
+
+z  = int(x) + int(y) #Converts the Strings to Integers
+print(z) # So z is not 24    it was 6!
+
+
+
+# Its Possible t arrang more inputs
+
+name = input()
+age = input()
+print(name + " ist" + age + "alt")
