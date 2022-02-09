a1d1f58e6365fd0555d9dbc6892d01e7b4d7da15
Fabian Suchan <fabian.suchan@adesso.de>
2022-02-09 08:55:49 +0100

    not importent

--- FlightTimeAufgabe.py
+++
@@ -1,8 +0,0 @@
-# Task Calcute the flight time of the upcoming trip
-# the distance is 7425 miles, and the Plane flies at the average speed of 550 Miles an Hour of 550 Miles an Hour
-# calculate the total flight time in Hours
-
-
-print(7425/550) 
-
-
